8f9498b82e53fbff9c413c0ce384eccce1df93db
Glider <358340995@qq.com>
2022-06-11 17:57:15 +0800

    init program

---
+++ OpenGL/src/Application.cpp
@@ -0,0 +1,14 @@
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+#include <iostream>
+
+#include "Application.h"
+
+int main()
+{
+	::std::cout << "Hello My OpenGL" << ::std::endl;
+
+	::std::cin.get();
+	return 0;
+}
